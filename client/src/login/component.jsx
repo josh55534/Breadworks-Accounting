2640daad22acd835793ceb8731067fc49d7cc5b0
Nick Merenda <nmerenda@students.kennesaw.edu>
2023-02-08 19:32:35 -0500

    Added LoginPage

--- client/src/login/component.jsx
+++ client/src/login/component.jsx
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function MyComponent() {
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:5000/api/login", {
+        email: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("token", res.data.token);
+        window.location.replace("/");
+      })
+      .catch((err) => {
+        setError(err.response.data.errors);
+      });
+  };
+
   return (
-    <div className="bg-red-500 p-4 text-white text-center">
-      This component has a red background.
+    <div className="flex h-screen bg-gray-200 justify-center items-center">
+      <form className="bg-white p-6 rounded-lg shadow-md" onSubmit={handleSubmit}>
+        <h2 className="text-lg font-medium mb-4">Login</h2>
+        <div className="mb-4">
+          <label className="block font-medium mb-2" htmlFor="username">
+            Email
+          </label>
+          <input
+            className="border border-gray-400 p-2 w-full"
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="border border-gray-400 p-2 w-full"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error ? (
+          <div className="text-red-500 font-medium mb-4">{error}</div>
+        ) : null}
+        <button className="bg-indigo-500 text-white py-2 px-4 rounded-full hover:bg-indigo-600">
+          Login
+        </button>
+      </form>
     </div>
   );
-}
+};
 
-export default MyComponent;
+export default LoginPage;
--- client/src/login/login.jsx
+++ client/src/login/login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import MyComponent from './component'
+import LoginPage from './component'
 import '../index.css'
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
-root.render(<MyComponent />);
+root.render(<LoginPage />);
