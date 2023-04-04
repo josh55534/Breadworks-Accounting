fb99ab4a4ecb380001f5fee8718431e6fc4b8c4e
josh55534 <joshuah.collins@hotmail.com>
2023-04-04 15:12:07 -0400

    added compound journal entry creation

--- client/src/pages/journalizing/journalCreate.jsx
+++ client/src/pages/journalizing/journalCreate.jsx
@@ -10,14 +10,16 @@ function CreateJournal() {
   const [accountList, setAccountList] = useState([])
   const [accountListID, setAccountListId] = useState([])
 
-  const [debitAccountID, setDebitID] = useState("none");
-  const [creditAccountID, setCreditID] = useState("");
+  const [rowID, setRowID] = useState([0])
+  const [accountID, setAccountID] = useState(["none"]);
+  const [creditAmount, setCredit] = useState([])
+  const [debitAmount, setDebit] = useState([]);
   const [desc, setDesc] = useState("");
   const [date, setDate] = useState("");
   const [amount, setAmount] = useState();
   const [file, setFile] = useState("");
   const [userName, setUsername] = useState("")
-  
+
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -43,48 +45,117 @@ function CreateJournal() {
 
   }, [config])
 
+  const handleSubmit = () => {
+
+  }
+
+  const changeAccount = (props) => {
+    var account = [...accountID]
+    account[props.target.id] = props.target.value;
+    setAccountID(account)
+  }
+
+  const removeList = (props) => {
+    if (rowID.length > 1) {
+      var tempRowID, tempAccountID, tempCreditAmount, tempDebitAmount;
+      tempRowID = [...rowID];
+      tempAccountID = [...accountID];
+      tempCreditAmount = [...creditAmount];
+      tempDebitAmount = [...debitAmount];
+
+      tempRowID.splice(props.target.id, 1);
+      tempAccountID.splice(props.target.id, 1);
+      tempCreditAmount.splice(props.target.id, 1);
+      tempDebitAmount.splice(props.target.id, 1);
+
+      setRowID(tempRowID);
+      setAccountID(tempAccountID)
+      setDebit(tempDebitAmount)
+      setCredit(tempCreditAmount)
+    }
+  }
+
+  const addList = (props) => {
+    var tempRowID, tempAccountID, tempCreditAmount, tempDebitAmount;
+    tempRowID = [...rowID];
+    tempAccountID = [...accountID];
+    tempCreditAmount = [...creditAmount];
+    tempDebitAmount = [...debitAmount];
+
+    tempRowID.push(tempRowID.length)
+    tempAccountID.push("none")
+    tempCreditAmount.push("");
+    tempDebitAmount.push("");
+
+    setRowID(tempRowID);
+    setAccountID(tempAccountID)
+    setDebit(tempDebitAmount)
+    setCredit(tempCreditAmount)
+  }
+
   return (
     <div className="window-primary">
       <h2>New Journal Entry</h2>
       <form>
         <div className="form-primary">
           <div>
-            <label>
-              Debit Account
-            </label>
-            <select
-              className="txt-primary"
-              type="text"
-              id="debit"
-              value={debitAccountID}
-              onChange={(props) => setDebitID(props.target.value)}
-            >
-              <option value="none" disabled hidden>Select an Option</option>
-              {accountListID.map((id, index) => {
-                return (
-                  <option value={id} key={index}>{accountList[index]}</option>
-                )
-              })}
-            </select>
-          </div>
-          <div>
-            <label>
-              Credit Account
-            </label>
-            <select
-              className="txt-primary"
-              type="text"
-              id="credit"
-              value={creditAccountID}
-              onChange={(props) => setCreditID(props.target.value)}
-            >
-              <option value="none" disabled hidden>Select an Option</option>
-              {accountListID.map((id, index) => {
-                return (
-                  <option value={id} key={index}>{accountList[index]}</option>
-                )
-              })}
-            </select>
+            <table className="user-table">
+              <thead>
+                <tr>
+                  <td className="user-table-header text-black">
+                    <label>Account</label>
+                  </td>
+                  <td className="user-table-header text-black">
+                    <label>Debit</label>
+                  </td>
+                  <td className="user-table-header text-black">
+                    <label>Credit</label>
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                {rowID.map((d, index) => (
+                  <tr key={index}>
+                    <td className="user-table-body">
+                      <select
+                        className="txt-primary ml-0"
+                        type="text"
+                        id={index}
+                        value={accountID[index]}
+                        onChange={changeAccount}
+                      >
+                        <option value="none" disabled hidden>Select an Option</option>
+                        {accountListID.map((id, index) => {
+                          return (
+                            <option value={id} key={index}>{accountList[index]}</option>
+                          )
+                        })}
+                      </select>
+                    </td>
+                    <td className="user-table-body">
+                      <input
+                        className="txt-primary ml-0"
+                        type="text"
+                        id={index}
+                        value={debitAmount[index]}
+                      />
+                    </td>
+                    <td className="user-table-body">
+                      <input
+                        className="txt-primary ml-0"
+                        type="text"
+                        id={index}
+                        value={creditAmount[index]}
+                      />
+                    </td>
+                    <td>
+                      <button type="button" id={index} className="btn-primary btn-color-red" onClick={removeList}>-</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button type="button" className="btn-primary" onClick={addList}>+</button>
           </div>
           <div>
             <label>
@@ -138,8 +209,8 @@ function CreateJournal() {
           <Link to="/journal/entries" className="btn-primary btn-color-red">
             Go Back
           </Link>
-          <button className="btn-primary">
-            Add Account
+          <button className="btn-primary" onClick={handleSubmit}>
+            Add Entry
           </button>
         </div>
       </form >
