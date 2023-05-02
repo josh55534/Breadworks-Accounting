d458d1fd1eded4c03a82030fd2d8b93b92701a05
josh55534 <joshuah.collins@hotmail.com>
2023-05-02 10:56:42 -0400

    fixed document link

--- client/src/header.jsx
+++ client/src/header.jsx
@@ -12,56 +12,56 @@ function HeadNavbar() {
   }
 
 
-	return (
-		<div>
-			<div className="flex flex-row justify-center gap-10">
-				{token && (
-					<>
-						<Link to="/chartofaccounts">
-							<button className="btn-navbar">
-								Chart of Accounts
-							</button>
-						</Link>
-						<Link to="/journal">
-							<button className="btn-navbar">
-								Journal
-							</button>
-						</Link>
+  return (
+    <div>
+      <div className="flex flex-row justify-center gap-10">
+        {token && (
+          <>
+            <Link to="/chartofaccounts">
+              <button className="btn-navbar">
+                Chart of Accounts
+              </button>
+            </Link>
+            <Link to="/journal">
+              <button className="btn-navbar">
+                Journal
+              </button>
+            </Link>
             <Link to="/eventlog">
-							<button className="btn-navbar">
-								Event Log
-							</button>
-						</Link>
-					</>
-				)}
-				{decoded && decoded.user.role === "admin" && (
+              <button className="btn-navbar">
+                Event Log
+              </button>
+            </Link>
+          </>
+        )}
+        {decoded && decoded.user.role === "admin" && (
 
-					<Link to='/admindashboard'>
-						<button className="btn-navbar"
-							onClick={() => {
-							}}
-						>
-							Admin Dashboard
-						</button>
-					</Link>
-)}
-{decoded && decoded.user.role === "manager" && (
-  <Link to=''>
-    <button className="btn-navbar"
-    >
-      Documents
-    </button>
-  </Link>
-)}
+          <Link to='/admindashboard'>
+            <button className="btn-navbar"
+              onClick={() => {
+              }}
+            >
+              Admin Dashboard
+            </button>
+          </Link>
+        )}
+        {decoded && decoded.user.role === "manager" && (
+          <Link to='/documents'>
+            <button className="btn-navbar"
+            >
+              Documents
+            </button>
+          </Link>
+        )}
 
-				{decoded && decoded.user.verify === "unverified" && (
-					<div className="mx-auto bg-red-600 text-slate-100 w-80 mt-4">
-						You will recieve an email at {decoded.user.email} after an Admin verifies your account
-					</div>
-				)}
-			</div>
-		</div>
-	);
+        {decoded && decoded.user.verify === "unverified" && (
+          <div className="mx-auto bg-red-600 text-slate-100 w-80 mt-4">
+            You will recieve an email at {decoded.user.email} after an Admin verifies your account
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 function Logo(props) {
